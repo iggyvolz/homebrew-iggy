cb1012d1eab042a4e4340f188a3d725ff9317012
iggyvolz <iggyvolz@gmail.com>
2015-02-09 19:44:52 -0500

    Update adminer

--- adminer.rb
+++ adminer.rb
@@ -1,10 +1,12 @@
 class Adminer < Formula
   homepage "http://www.adminer.org/"
-  url "http://downloads.sourceforge.net/adminer/adminer-4.2.0.php"
+  url "http://downloads.sourceforge.net/adminer/adminer-#{version}.php"
   version "4.2.0"
   sha1 "4676730cd7e30a0bb2b6ecc1fa20bfefead11854"
 
   def install
+    system "rm", "-f", "/usr/local/apache2/htdocs/adminer.php"
+    system "cp", "/Library/Caches/Homebrew/adminer-#{version}.php", "/usr/local/apache2/htdocs/adminer.php"
   end
 
   test do
