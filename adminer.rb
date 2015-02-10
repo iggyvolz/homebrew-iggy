dfdd8e6e9d8af471a1d918f70f65ddd7f18e1e14
iggyvolz <iggyvolz@gmail.com>
2015-02-09 19:31:23 -0500

    Add adminer

---
+++ adminer.rb
@@ -0,0 +1,13 @@
+class Adminer < Formula
+  homepage "http://www.adminer.org/"
+  url "http://downloads.sourceforge.net/adminer/adminer-4.2.0.php"
+  version "4.2.0"
+  sha1 "4676730cd7e30a0bb2b6ecc1fa20bfefead11854"
+
+  def install
+  end
+
+  test do
+    system "true"
+  end
+end
